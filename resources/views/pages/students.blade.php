d3093d1c354c0db28e009fed8c591afb9ff0ff57
tvy22 <julysok2004@gmail.com>
2026-01-22 12:54:22 +0700

    update

--- resources/views/pages/students.blade.php
+++ resources/views/pages/students.blade.php
@@ -107,7 +107,7 @@
 
                 <button @click="showRegisterModal = true" class="flex items-center gap-2 px-5 py-2.5 bg-blue-600 text-white rounded-2xl font-black text-sm hover:bg-blue-700 transition shadow-lg shadow-blue-100 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
-                    <span>Register Student</span>
+                    <span>Add Student</span>
                 </button>
             </div>
         </div>
