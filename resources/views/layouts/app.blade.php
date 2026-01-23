84c4beb49cd0e65add31066660a170cda44318c4
tvy22 <julysok2004@gmail.com>
2026-01-23 23:29:27 +0700

    add attendance menu button in dashboard sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,9 +113,18 @@
                 <a href="/students"
                    class="group flex items-center gap-4 px-5 py-3 transition-all duration-200 transform active:scale-95 rounded-xl font-bold {{ request()->is('students') ? 'bg-white text-[#554DDE] shadow-xl scale-[1.02]' : 'text-white/90 hover:text-white hover:bg-white/10 hover:translate-x-1' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                     </svg>
                     Students
+                </a>
+
+                {{-- Attendances --}}
+                <a href="/attendances"
+                   class="group flex items-center gap-4 px-5 py-3 transition-all duration-200 transform active:scale-95 rounded-xl font-bold {{ request()->is('attendances') ? 'bg-white text-[#554DDE] shadow-xl scale-[1.02]' : 'text-white/90 hover:text-white hover:bg-white/10 hover:translate-x-1' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.375M9 18h3.375m2.25-13.5h1.125a2.25 2.25 0 0 1 2.25 2.25v13.5a2.25 2.25 0 0 1-2.25 2.25h-11.25a2.25 2.25 0 0 1-2.25-2.25V6.75a2.25 2.25 0 0 1 2.25-2.25h1.125m.75-1.5h7.5a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3.75a.75.75 0 0 1 .75-.75Z" />
+                    </svg>
+                    Attendances
                 </a>
 
                 {{-- About Us --}}
